af3dcb784b181c6d6bf9ff5fc786b5880b338cec
EdivaldoCostaLimaJr <edivaldocostadv@gmail.com>
2022-07-19 12:25:34 -0300

    HTML/CSS finished

---
+++ script.js
@@ -0,0 +1,16 @@
+//window.alert("Isso mesmo");
+
+const href = window.location.href;
+
+console.log(href);
+
+if(href === 'http://127.0.0.1:5500/index.html') {
+    console.log('É igual')
+}
+
+const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+h1Selecionado.addEventListener('click', function() {
+    console.log('Clicou em ', h1Selecionado.innerText);
+})
